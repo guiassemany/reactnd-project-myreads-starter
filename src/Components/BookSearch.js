82a98e459e661fe04bd96ed932bae902d39ba22d
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-01-20 17:41:37 -0200

    Refactoring: BookSearch Component

--- src/Components/BookSearch.js
+++ src/Components/BookSearch.js
@@ -1,38 +1,38 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import BookGrid from './BookGrid'
 import PropTypes from 'prop-types'
 import {DebounceInput} from 'react-debounce-input'
 
-class BookSearch extends Component {
+const BookSearch = (props) => {
 
-    render() {
-        const {onSearch, books, onUpdateBook, shelves} = this.props
+    const {onSearch, books, onUpdateBook, shelves} = props
 
-        return (
-            <div>
-                <div className="search-books">
-                    <div className="search-books-bar">
-                        <Link className="close-search" to='/'>Close</Link>
-                        <div className="search-books-input-wrapper">
-                            <DebounceInput
-                                minLength={3}
-                                debounceTimeout={500}
-                                onChange={event => onSearch(event.target.value)}
-                                placeholder="Search by title or author"/>
-                        </div>
-                    </div>
-                    <div className="search-books-results">
-                        {books.length > 0 && (<BookGrid books={books} shelves={shelves} onUpdateBook={onUpdateBook}/>)}
+    return (
+        <div>
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to='/'>Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <DebounceInput
+                            minLength={3}
+                            debounceTimeout={500}
+                            onChange={event => onSearch(event.target.value)}
+                            placeholder="Search by title or author"/>
                     </div>
                 </div>
+                <div className="search-books-results">
+                    {books.length > 0 && (<BookGrid books={books} shelves={shelves} onUpdateBook={onUpdateBook}/>)}
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 BookSearch.propTypes = {
     books: PropTypes.array,
+    shelves: PropTypes.array.isRequired,
+    updateBooks: PropTypes.func.isRequired,
     onSearch: PropTypes.func.isRequired
 }
 
