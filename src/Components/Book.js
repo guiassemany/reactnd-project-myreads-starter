51da3cea98e7b5a4bc9b42eb595435d378b9c82a
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-01-20 17:55:42 -0200

    Refactoring: Book Component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BookShelfChanger from './BookShelfChanger'
 import PropTypes from 'prop-types'
 
-class Book extends Component {
-    render() {
-        const {book, shelves, onUpdateBook} = this.props
+const Book = (props) => {
+    const {book, shelves, onUpdateBook} = props
 
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url("${book.imageLinks.thumbnail}")`
-                    }}></div>
-                    <BookShelfChanger book={book} shelves={shelves} onUpdateBook={onUpdateBook}/>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                }}></div>
+                <BookShelfChanger book={book} shelves={shelves} onUpdateBook={onUpdateBook}/>
             </div>
-        )
-    }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+        </div>
+    )
 }
 
 Book.propTypes = {
-    coverURL: PropTypes.string,
-    title: PropTypes.string,
-    authors: PropTypes.string
+    book: PropTypes.object.isRequired,
+    shelves: PropTypes.array.isRequired,
+    onUpdateBook: PropTypes.func.isRequired
 }
 
 export default Book
