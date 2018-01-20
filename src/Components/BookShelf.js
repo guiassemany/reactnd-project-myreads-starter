9ae986e3678a56fcc925ca48e1d9116a7839533a
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-01-20 17:38:38 -0200

    Refactoring: BookShelf Component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,22 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BookGrid from './BookGrid'
+import PropTypes from 'prop-types'
 import * as Helpers from '../utils/Helpers'
 
-class BookShelf extends Component {
-    render() {
-        const {books, shelves, shelf, onUpdateBook} = this.props
-
-        return (
-            <div>
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">{Helpers.camelCaseToReadable(shelf)}</h2>
-                    <div className="bookshelf-books">
-                        <BookGrid shelves={shelves} books={books} onUpdateBook={onUpdateBook}/>
-                    </div>
+const BookShelf = (props) => {
+    const {books, shelves, shelf, onUpdateBook} = props
+    return (
+        <div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{Helpers.camelCaseToReadable(shelf)}</h2>
+                <div className="bookshelf-books">
+                    <BookGrid shelves={shelves} books={books} onUpdateBook={onUpdateBook}/>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
+
+BookShelf.propTypes = {
+    books: PropTypes.array.isRequired,
+    shelves: PropTypes.array.isRequired,
+    shelf: PropTypes.string.isRequired,
+    onUpdateBook: PropTypes.func.isRequired
 }
 
 export default BookShelf
