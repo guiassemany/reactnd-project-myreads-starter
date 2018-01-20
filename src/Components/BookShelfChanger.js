813df41f96fc8127b0a72b32cdbcfccee7667c7a
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-01-20 17:30:16 -0200

    Refactoring: BookShelfChanger Component

--- src/Components/BookShelfChanger.js
+++ src/Components/BookShelfChanger.js
@@ -1,34 +1,24 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import * as Helpers from '../utils/Helpers'
 
-class BookShelfChanger extends Component {
+const BookShelfChanger = (props) => {
+    const {book, shelves, onUpdateBook} = props
 
-    state = {
-        book: {}
+    const handleShelfChange = (e) => {
+        onUpdateBook(book, e.target.value)
     }
 
-    componentDidMount() {
-        this.setState({book: this.props.book})
-    }
-
-    handleShelfChange = (e) => {
-        this.props.onUpdateBook(this.state.book, e.target.value)
-    }
-
-    render() {
-        const {book} = this.props
-        return (
-            <div className="book-shelf-changer">
-                <select onChange={this.handleShelfChange} value={book.shelf ? book.shelf : 'none'}>
-                    <option value="none" disabled>Move to...</option>
-                    {this.props.shelves.map(shelf => (
-                        <option key={shelf} value={shelf}>{Helpers.camelCaseToReadable(shelf)}</option>
-                    ))}
-                </select>
-            </div>
-        )
-    }
+    return (
+        <div className="book-shelf-changer">
+            <select onChange={handleShelfChange} value={book.shelf ? book.shelf : 'none'}>
+                <option value="none" disabled>Move to...</option>
+                {shelves.map(shelf => (
+                    <option key={shelf} value={shelf}>{Helpers.camelCaseToReadable(shelf)}</option>
+                ))}
+            </select>
+        </div>
+    )
 }
 
 BookShelfChanger.propTypes = {
