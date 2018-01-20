28553c2ac8d3be42c8f764115c75a6d06fb5cf11
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-01-20 17:49:16 -0200

    Refactoring: BookList component

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,33 +1,31 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BookShelf from './BookShelf'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-class BookList extends Component {
-    render() {
-        const {shelves, getAllFromShelf, onUpdateBook} = this.props
+const BookList = (props) => {
+    const {shelves, getAllFromShelf, onUpdateBook} = props
 
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    {
-                        shelves.map((shelf) => <BookShelf key={shelf}
-                                                          shelves={shelves}
-                                                          shelf={shelf}
-                                                          books={getAllFromShelf(shelf)}
-                                                          onUpdateBook={onUpdateBook}
-                        />)
-                    }
-                </div>
-                <div className="open-search">
-                    <Link to='/search'>Add a book</Link>
-                </div>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
             </div>
-        )
-    }
+            <div className="list-books-content">
+                {
+                    shelves.map((shelf) => <BookShelf key={shelf}
+                                                      shelves={shelves}
+                                                      shelf={shelf}
+                                                      books={getAllFromShelf(shelf)}
+                                                      onUpdateBook={onUpdateBook}
+                    />)
+                }
+            </div>
+            <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+            </div>
+        </div>
+    )
 }
 
 BookList.propTypes = {
